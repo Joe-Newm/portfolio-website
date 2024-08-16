983bf44a0b20a5023a1a962a0a704ca56a3b7190
unknown <j.newm500@gmail.com>
2024-08-16 00:01:54 -0500

    add get in touch, change styles, convert to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class', // Enable dark mode using the 'class' strategy
 	theme: {
 		extend: {
-			fontFamily: {
-				"custom": ["inter", "sans-serif"]
-			}
+			colors: {
+				light: '#f1f1f1', // Custom light mode color
+				dark: '#1a1a1a', // Custom dark mode color
+			},
 		},
 	},
 	plugins: [],
-}
+};
